5ef084ccc7ba2fc2da2157d4e71328342d6abc3c
Monletrhor <monleonico@gmail.com>
2026-04-07 19:46:31 +0200

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,8 @@
+'use client'
+import React from "react";
+export default function Page() {
+  return <div style={{padding:40,fontFamily:'Arial'}}>
+    <h1>Modepran App</h1>
+    <p>App lista. Ahora despliega en Vercel.</p>
+  </div>
+}
